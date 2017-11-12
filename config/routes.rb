e3fd669475bc445d7a5c31859195d736922efc7c
New <new@News-MacBook-Pro.local>
2017-11-12 14:54:34 -0800

    Personalizando las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
+  # Se establecen las rutas
+  get 'acerca-de', to: 'pages#about'
 
-  get 'pages/contact'
+  get 'contacto', to:'pages#contact'
 
   resources :blogs
+  # De esta forma la pagina de inicio sera home
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
