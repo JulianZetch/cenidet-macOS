bb2d6f4935cfe339ebf14b009043d46e7065830b
New <new@News-MacBook-Pro.local>
2017-11-11 21:31:24 -0800

    Funciones de editar y actualizar

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,6 +21,24 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+    # El metodo editar a diferencia del new, necesita un id para saber cual editar
+    # por ejemplo una ruta seria : http://localhost:3000/portfolios/5/edit
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item .update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Cambios del articulo guardados' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        #format.json { render json: @portfolio_item .errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
 
 
