498ede351af0a0984b87c287de375888a1b23faf
New <new@News-MacBook-Pro.local>
2017-11-12 21:21:45 -0800

    Friendly Routes para los Post del Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
+# Para mas informacion visitala guia de FriendlyId: https://github.com/norman/friendly_id
 class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged # Ahora nuestra url va a combinarse con el titulo del Blog!
 end
