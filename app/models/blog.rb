d32545fc18e96732352b9bfaa6272ad9aa0b2ec3
New <new@News-MacBook-Pro.local>
2017-11-13 18:25:17 -0800

    Implementando validacion de Blogs, Articulos de portafolio y Habilidades

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
 # Para mas informacion visitala guia de FriendlyId: https://github.com/norman/friendly_id
   friendly_id :title, use: :slugged # Ahora nuestra url va a combinarse con el titulo del Blog!
+
+ # Para validar que el blog contrenga Titulo y Cuerpo
+ validates_presence_of :title, :body
 end
