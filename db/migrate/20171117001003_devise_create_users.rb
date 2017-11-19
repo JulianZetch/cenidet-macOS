a231e13f09b6af3ed8c535420e456e09411c9545
New <new@News-MacBook-Pro.local>
2017-11-19 12:08:33 -0800

    Cambios menores

--- db/migrate/20171117001003_devise_create_users.rb
+++ db/migrate/20171117001003_devise_create_users.rb
@@ -6,7 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :encrypted_password, null: false, default: ""
 
       ## Custom fields
-      t.string :name
+      t.string :name, null: false
+      ## Agregamos el apellido al Usario
+      ##t.string :last_name
+
 
       ## Recoverable
       t.string   :reset_password_token
